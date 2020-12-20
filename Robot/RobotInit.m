27a7a12ca75d6056c0510fd825a643b00cdcd58e
malustewart <mastewart@itba.edu.ar>
2020-12-20 17:50:49 -0300

    disenio de robot

--- Robot/RobotInit.m
+++ Robot/RobotInit.m
@@ -1,4 +1,4 @@
-function arm = RobotInit(pert)
+function arm = RobotInit(pert, qlim)
     N = 4; % cantidad de links
 
     L1 = 0;
@@ -23,7 +23,7 @@ function arm = RobotInit(pert)
     DH = struct('d', cell(1,N), 'a', cell(1,N), 'alpha', cell(1,N), 'theta', cell(1,N),...
         'type', cell(1,N)); %genera estructura base
     DH(1).alpha = 0;    DH(1).a = L1;    DH(1).d = 0;    DH(1).type = 'R';
-    DH(2).alpha = pi/2;    DH(2).a = L2;   DH(2).d = 0;    DH(2).type = 'R';
+    DH(2).alpha = pi/2; DH(2).a = L2;   DH(2).d = 0;    DH(2).type = 'R';
     DH(3).alpha = 0;    DH(3).a = L3;   DH(3).d = 0;    DH(3).type = 'R';
     DH(4).alpha = 0;    DH(4).a = L4;   DH(4).d = 0;    DH(4).type = 'R';
 
@@ -35,7 +35,8 @@ function arm = RobotInit(pert)
     %Tool = transl(Lee, 100, 0); % Offset de la herramienta
     Tool = [0 1 0 Lee; -1 0 0 -100; 0 0 1 0; 0 0 0 1];
     Base = [0 0 130];
+    %qlim = [0 2*pi; 0.8446 pi-0.8446 ; -2.3454 0; 0 pi*3/4 ];
 
-    arm = SerialLink([links{:}], 'tool', Tool, 'name', 'messi', 'base', Base);
+    arm = SerialLink([links{:}], 'tool', Tool, 'name', 'messi', 'base', Base);%, 'qlim', qlim);
 
 end
